6f5775ac512e563407bc756db9d9655de466ad1d
AnkanShahriar <60335513+AnkanShahriar@users.noreply.github.com>
2021-01-05 20:10:20 +0600

    Ankan

---
+++ ankan/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZenBD_API.Authentication;
+using ZenBD_API.Models;
+using ZenBD_API.Repositories;
+
+namespace ZenBD_API.Controllers
+{
+    [RoutePrefix("api/categories")]
+    public class CategoryController : ApiController
+    {
+        
+          CategoryRepository CatRepo = new CategoryRepository();
+
+            [Route("")]
+            public IHttpActionResult Get()
+            {
+                return Ok(CatRepo.GetAll());
+            }
+
+            [Route("{id}")]
+            public IHttpActionResult Get(int id)
+            {
+                var user = CatRepo.Get(id);
+                if (user == null)
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                return Ok(CatRepo.Get(id));
+            }
+            [Route("")]
+            public IHttpActionResult Post(Category category)
+            {
+                CatRepo.Insert(category);
+                return Created("api/categories" + category.CategoryId, category);
+            }
+            [Route("{id}")]
+            public IHttpActionResult Put([FromUri] int id, [FromBody] Category category)
+            {
+                category.CategoryId = id;
+                CatRepo.Update(category);
+                return Ok(category);
+            }
+            [Route("{id}")]
+            public IHttpActionResult Delete(int id)
+            {
+                var user = CatRepo.Get(id);
+                if (user == null)
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                CatRepo.Delete(id);
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+    }
+}
---
+++ ankan/Controllers/ContactController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZenBD_API.Authentication;
+using ZenBD_API.Models;
+using ZenBD_API.Repositories;
+
+namespace ZenBD_API.Controllers
+{
+    [RoutePrefix("api/contacts")]
+
+    public class ContactController : ApiController
+    {
+        ContactRepository ContactRepo = new ContactRepository();
+
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(ContactRepo.GetAll());
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            var user = ContactRepo.Get(id);
+            if (user == null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            return Ok(ContactRepo.Get(id));
+        }
+        [Route("")]
+        public IHttpActionResult Post(Contact contact)
+        {
+            ContactRepo.Insert(contact);
+            return Created("api/contacts" + contact.ContactId, contact);
+        }
+        [Route("{id}")]
+        public IHttpActionResult Put([FromUri] int id, [FromBody] Contact contact)
+        {
+            contact.ContactId = id;
+            ContactRepo.Update(contact);
+            return Ok(contact);
+        }
+        [Route("{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            var user = ContactRepo.Get(id);
+            if (user == null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            ContactRepo.Delete(id);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
